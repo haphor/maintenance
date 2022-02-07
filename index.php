f9f54a95b257d236335edfa8c25fee062ea78081
haphor <emmanuel.ola.afolabi@gmail.com>
2022-02-07 14:00:32 +0100

    Committing project files

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+    
+    <title>Accelerate Tv - Under Maintenance</title>
+    <meta name="description" content="Our site is under construction">
+    
+    <!--Favicon-->
+    <link rel="shortcut icon" href="./images/favicon1.png" type="image/x-icon" />
+    
+    <!-- Font Awesome-->
+    <link href="font/font-awesome-4.7.0/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+    
+    <!-- Bootstrap Core CSS-->
+    <link type="text/css" rel="stylesheet" href="./css/bootstrap.min.css" media="screen,projection" />
+    
+    <!-- Material Design Bootstrap CSS -->
+    <link type="text/css" rel="stylesheet" href="./css/mdb.min.css" media="screen,projection" />
+    
+    <!-- My custom CSS -->
+    <link type="text/css" rel="stylesheet" href="./css/style.css" media="screen,projection" />
+</head>
+
+<body>
+    <section id="maintenance" class="d-flex align-items-center h-100 py-md-0 py-4">
+        <div class="container">
+            <div class="d-flex flex-column flex-nowrap justify-content-around align-items-center align-content-center py-2 py-md-5">
+                <img src="images/logo.png" alt="Accelerate Tv Logo" />
+                <img src="images/maintenance-image.png" alt="Maintenance Image" />
+                <h1 class="mb-3">Under Maintenance</h1>
+                <p>
+                    We’re working to make our site experience more rewarding.
+                    Please give us a moment, we’ll be back up as soon as possible.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <!--jQuery first-->
+    <script type="text/javascript" src="./js/jquery-3.3.1.min.js"></script>
+    <!-- jQuery Ui -->
+    <script type="text/javascript" src="./js/jquery-ui.min.js"></script>
+    <!-- jQuery migrate -->
+    <script src="https://code.jquery.com/jquery-migrate-1.4.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="./js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+    <!-- Material Design Bootstrap Javascript -->
+    <script type="text/javascript" src="./js/mdb.min.js"></script>
+    <!-- custom Javascript -->
+    <script type="text/javascript" src="./js/main.js"></script>
+
+</body>
+
+</html>
